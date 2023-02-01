b76ddc8853845b5154b2af62d7c2163f567cae50
shivammane29 <124154878+shivammane29@users.noreply.github.com>
2023-02-01 18:19:57 +0530

    Add files via upload

---
+++ Login_Page/src/main/java/LoginServlet.java
@@ -0,0 +1,35 @@
+
+//import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+  
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname= request.getParameter("username");
+		String pass= request.getParameter("password");
+		
+		if(uname.equals("Shivam") && pass.equals("12345")) {
+	
+			response.sendRedirect("wellcome.jsp");
+					
+		}
+
+	}
+
+	
+
+}
